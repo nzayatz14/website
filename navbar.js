fdc8a96691a313a5c7e3c54123447190ac831a20
Nick Zayatz <nzayatz@cirtual.com>
2016-05-28 15:48:13 -0500

    added nav bar and laid out site

---
+++ navbar.js
@@ -0,0 +1,6 @@
+document.getElementById("navMenu").innerHTML =
+ '<ul>'+
+  '<li><a href="index.html">Home</a></li>'+
+  '<li><a href="projects.html">Projects</a></li>'+
+  '<li><a href="about.html">About</a></li>'+
+ '</ul>';
