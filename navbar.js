b73dcdae97d96e4424b50345a3c4998b3a08cf50
Nick Zayatz <nzayatz@cirtual.com>
2016-05-31 15:18:26 -0500

    navBar css

--- navbar.js
+++ navbar.js
@@ -2,5 +2,7 @@ document.getElementById("navMenu").innerHTML =
  '<ul>'+
   '<li><a href="index.html">Home</a></li>'+
   '<li><a href="projects.html">Projects</a></li>'+
-  '<li><a href="about.html">About</a></li>'+
+  '<li><a href="http://github.com/nzayatz14">Github</a></li>'+
+  '<li><a href="Resume.pdf">Resume</a></li>'+
+  '<li style="float:right"><a href="http://www.cirtual.com/">Cirtual</a></li>'
  '</ul>';
